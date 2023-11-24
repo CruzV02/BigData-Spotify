62c9f2912f384d4bf1bb3c3c5d3f914bd6868044
José Rubén <123268828+CruzV02@users.noreply.github.com>
2023-11-23 20:28:14 -0600

    Add files via upload
    Implementación de Flask y diseño inicial

--- Spotify/Letras.py
+++ Spotify/Letras.py
@@ -1,7 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 import spotipy
 import os
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
+
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -10,6 +15,7 @@ load_dotenv()
 os.environ["SPOTIPY_CLIENT_ID"] = os.getenv("SPOTIPY_CLIENT_ID")
 os.environ["SPOTIPY_CLIENT_SECRET"] = os.getenv("SPOTIPY_CLIENT_SECRET")
 os.environ["SPOTIPY_REDIRECT_URI"] = os.getenv("SPOTIPY_REDIRECT_URI")
+
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
@@ -36,7 +42,9 @@ if len(items) > 0:
         # Obtener álbum
         tracks = spotify.album_tracks(album_id)['items']
 
-        # Imprimir canciones
+        
+
+    ''' Imprimir canciones
         print(f"Canciones del álbum '{album_name}':")
         for track in tracks:
             print(track['name'])
@@ -44,3 +52,36 @@ if len(items) > 0:
         print(f"No se encontró el álbum '{album_name}' para el artista '{artist_name}'.")
 else:
     print(f"No se encontró al artista '{artist_name}'.")
+'''
+def get_album_image_url(artist_name, album_name):
+    results = spotify.search(q='artist:' + artist_name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+
+        # Obtener los álbumes del artista
+        albums = spotify.artist_albums(artist['id'], album_type='album')['items']
+
+        # Buscar el ID del álbum deseado
+        album_id = None
+        for album in albums:
+            if album['name'] == album_name:
+                album_id = album['id']
+                break
+
+        if album_id:
+            # Obtener álbum
+            album = spotify.album(album_id)
+            return album['images'][0]['url']
+    
+    return None
+
+
+@app.route('/')
+def index():
+    # Renderiza el template con la lista de canciones y la URL de la imagen de fondo
+    image_url = get_album_image_url(artist_name, album_name)
+    return render_template('canciones.html', artist_name=artist_name, album_name=album_name, tracks=tracks, image_url=image_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
