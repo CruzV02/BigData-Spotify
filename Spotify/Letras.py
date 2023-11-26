c90306333c2faab4e3b00cb34a7a775550a694a1
Nihil02 <244gggg@gmail.com>
2023-11-25 18:46:44 -0600

    TextBlob

--- Spotify/Letras.py
+++ Spotify/Letras.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 import spotipy
 import os
+import pandas as pd
+from textblob import TextBlob
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 from Lyrics import getLyrics
-import time
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -23,70 +24,75 @@ artist_name = input("Ingresa el nombre del artista: ")
 album_name = input("Ingresa el nombre del álbum: ")
 
 # Obtener el ID del artista
-results = spotify.search(q='artist:' + artist_name, type='artist')
-items = results['artists']['items']
+results = spotify.search(q="artist:" + artist_name, type="artist")
+items = results["artists"]["items"]
 if len(items) > 0:
-    artist_id = items[0]['id']
+    artist_id = items[0]["id"]
 
     # Obtener los álbumes del artista
-    albums = spotify.artist_albums(artist_id, album_type='album')['items']
+    albums = spotify.artist_albums(artist_id, album_type="album")["items"]
 
     # Buscar el ID del álbum deseado
     album_id = None
     for album in albums:
-        if album['name'] == album_name:
-            album_id = album['id']
+        if album["name"] == album_name:
+            album_id = album["id"]
             break
 
     if album_id:
         # Obtener álbum
-        tracks = spotify.album_tracks(album_id)['items']
+        tracks = spotify.album_tracks(album_id)["items"]
+
 
 def get_album_image_url(artist_name, album_name):
-    results = spotify.search(q='artist:' + artist_name, type='artist')
-    items = results['artists']['items']
+    results = spotify.search(q="artist:" + artist_name, type="artist")
+    items = results["artists"]["items"]
     if len(items) > 0:
         artist = items[0]
 
         # Obtener los álbumes del artista
-        albums = spotify.artist_albums(artist['id'], album_type='album')['items']
+        albums = spotify.artist_albums(artist["id"], album_type="album")["items"]
 
         # Buscar el ID del álbum deseado
         album_id = None
         for album in albums:
-            if album['name'] == album_name:
-                album_id = album['id']
+            if album["name"] == album_name:
+                album_id = album["id"]
                 break
 
         if album_id:
             # Obtener álbum
             album = spotify.album(album_id)
-            return album['images'][0]['url']
+            return album["images"][0]["url"]
 
     return None
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    # Obtener álbum
-    #album = spotify.album(album_id)
-    #tracks = spotify.album_tracks(album_id)['items']
+    tracks = spotify.album_tracks(album_id)["items"]
     lyrics = []
     for track in tracks:
-        lyrics.append(getLyrics(artist_name, track['name']))
-        print(track['name'])
+        lyrics.append(getLyrics(artist_name, track["name"]))
+        print(track["name"])
 
+    data = pd.DataFrame(lyrics, columns=["lyrics", "para"])
+    data.lyrics = data.lyrics.astype(str)
+    data[["polarity", "subjectivity"]] = data["lyrics"].apply(
+        lambda Text: pd.Series(TextBlob(Text).sentiment)
+    )
+    print(data.head())
 
-    # Obtener la primera canción
-    #first_track = tracks[0]
-
-    # Obtener letras solo para la primera canción
-    #lyrics = getLyrics(artist_name, first_track['name'])
-
-    # Imprimir el nombre de la primera canción
-    #print(first_track['name'])
-    # Renderizar el template con la lista de canciones, la URL de la imagen de fondo y las letras
     image_url = get_album_image_url(artist_name, album_name)
-    return render_template('canciones.html', artist_name=artist_name, album_name=album_name, tracks=tracks, image_url=image_url, lyrics=lyrics)
+    return render_template(
+        "canciones.html",
+        artist_name=artist_name,
+        album_name=album_name,
+        tracks=tracks,
+        image_url=image_url,
+        lyrics=lyrics,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
