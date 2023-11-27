12e3f134baac3e3ec00b05936bc7e184983b6409
Nihil02 <244gggg@gmail.com>
2023-11-26 21:27:43 -0600

    Mucha chamba, ya medio quedó la página del artista, album y falta la de canción además de los datos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from dotenv import load_dotenv
 from flask import Flask
 
@@ -14,6 +15,9 @@ os.environ["SPOTIPY_REDIRECT_URI"] = os.getenv("SPOTIPY_REDIRECT_URI")
 
 app = Flask(__name__)
 app.config["FLASK_DEBUG"] = True
+app.config["SECRET_KEY"] = secrets.token_urlsafe(20)
+
+#Configuramos las rutas de la app
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
--- routes/routes.py
+++ routes/routes.py
@@ -1,83 +1,69 @@
-from flask import Blueprint, render_template
-from functions.Lyrics import getLyrics
+from flask import Blueprint, abort, redirect, render_template, url_for
+from forms import SearchForm
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import pandas as pd
-from textblob import TextBlob
 
 routes = Blueprint("routes", __name__, template_folder="templates")
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
-artist_name = input("Ingresa el nombre del artista: ")
-album_name = input("Ingresa el nombre del álbum: ")
 
-# Obtener el ID del artista
-results = spotify.search(q="artist:" + artist_name, type="artist")
-items = results["artists"]["items"]
-if len(items) > 0:
-    artist_id = items[0]["id"]
-
-    # Obtener los álbumes del artista
-    albums = spotify.artist_albums(artist_id, album_type="album")["items"]
-
-    # Buscar el ID del álbum deseado
-    album_id = None
-    for album in albums:
-        if album["name"] == album_name:
-            album_id = album["id"]
-            break
-
-    if album_id:
-        # Obtener álbum
-        tracks = spotify.album_tracks(album_id)["items"]
-
-
-def get_album_image_url(artist_name, album_name):
-    results = spotify.search(q="artist:" + artist_name, type="artist")
-    items = results["artists"]["items"]
-    if len(items) > 0:
-        artist = items[0]
-
-        # Obtener los álbumes del artista
-        albums = spotify.artist_albums(artist["id"], album_type="album")["items"]
-
-        # Buscar el ID del álbum deseado
-        album_id = None
-        for album in albums:
-            if album["name"] == album_name:
-                album_id = album["id"]
-                break
-
-        if album_id:
-            # Obtener álbum
-            album = spotify.album(album_id)
-            return album["images"][0]["url"]
-
-    return None
-
-
-@routes.route("/")
+@routes.route("/", methods=["POST", "GET"])
 def index():
-    tracks = spotify.album_tracks(album_id)["items"]
-    lyrics = []
-    for track in tracks:
-        lyrics.append(getLyrics(artist_name, track["name"]))
-        print(track["name"])
+    form = SearchForm()
+    error = None
+    if form.validate_on_submit():
+        artist = form.artist.data
+        return redirect("/" + artist + "/")
+    return render_template("index.html", form=form, error=error)
 
-    data = pd.DataFrame(lyrics, columns=["lyrics", "para"])
-    data.lyrics = data.lyrics.astype(str)
-    data[["polarity", "subjectivity"]] = data["lyrics"].apply(
-        lambda Text: pd.Series(TextBlob(Text).sentiment)
-    )
-    print(data.head())
 
-    image_url = get_album_image_url(artist_name, album_name)
+@routes.route("/<string:artist>/")
+def artist_page(artist):
+    results = spotify.search(q="artist:" + artist, type="artist")
+
+    if results["artists"]["total"] < 1:
+        print("Error")
+        return redirect(url_for("index"))
+        # abort(404)
+    else:
+        items = results["artists"]["items"]
+
+        print(items[0])
+
+        artist_id = items[0]["id"]
+        artist_name = items[0]["name"]
+        image_url = items[0]["images"][0]["url"]
+        albums = spotify.artist_albums(artist_id, album_type="album")["items"]
+        albums += spotify.artist_albums(artist_id, album_type="single")["items"]
     return render_template(
-        "canciones.html",
+        "artist.html",
+        artist_id=artist_id,
         artist_name=artist_name,
-        album_name=album_name,
+        albums=albums,
+        image_url=image_url,
+    )
+
+
+@routes.route("/<string:artist>/<string:album>/")
+def album_page(artist, album):
+    artist_name = spotify.artist(artist)["name"]
+    results = spotify.search(q=album + "artist:" + artist_name, type="album")
+    items = results["albums"]["items"]
+    print(items[0])
+
+    album_id = items[0]["id"]
+    image_url = items[0]["images"][0]["url"]
+    tracks = spotify.album_tracks(album_id)["items"]
+    return render_template(
+        "album.html",
+        album_id=album_id,
+        album_name=album,
         tracks=tracks,
         image_url=image_url,
-        lyrics=lyrics,
     )
+
+
+@routes.route("/<string:artist>/<string:album>/<string:track>/")
+def track_page(artist, album, track):
+    return render_template("testsubject.html")
